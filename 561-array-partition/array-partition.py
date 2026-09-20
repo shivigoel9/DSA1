c51acf87dade69e58c5e74ccadb88c3e73d2e740
Shivi Goel <shivigoel9a666@gmail.com>
2026-09-20 17:23:56 +0530

    Time: 32 ms (85.54%) | Memory: 13.7 MB (81.11%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        nums.sort()
+        total = 0
+
+        for i in range(0, len(nums), 2):
+            total += nums[i]
+        return total
